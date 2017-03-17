928e3351e1da02a8c1bc4c47fcdec1c572c3809e
234687552 <234687552@qq.com>
2017-03-17 15:11:56 +0800

    modify

--- angular-module/app/system/base/baseController.js
+++ angular-module/app/system/base/baseController.js
@@ -1,4 +1,4 @@
-
+// 存放基本共同的变量和方法
 'use strict';
 var app = angular.module('app', []);
 
--- angular-module/app/system/header/HeaderController.js
+++ angular-module/app/system/header/HeaderController.js
@@ -1,29 +1,32 @@
 app.controller('headCtrl', function ($scope, $location) {
     // Public.
     // "navItems"存储着导航栏的栏目。
+
+    console.log("headCtrl instance");
     $scope.navItems = [
         {
+            id: 1,
             title: 'Home',
             locationUrl: '/',
             href: '#'
         },
         {
+            id: 2,
             title: 'About Me',
             locationUrl: '/aboutMe',
             href: '#'
         },
         {
+            id: 3,
             title: 'Management',
             locationUrl: '/manageHome',
             href: '#'
         }
     ];
 
-    // // .selectedNavItem变量存储当前选择项，默认的选择项是"Home"。
-    // $scope.selectedItem = $scope.navItems[0];
     // 栏目click时触发的方法。
-    $scope.itemClick = function (navItem) {
-        $scope.selectedItem = navItem;
+    $scope.itemClick = function (id) {
+        $scope.selectedId = id;
     };
 
 });
