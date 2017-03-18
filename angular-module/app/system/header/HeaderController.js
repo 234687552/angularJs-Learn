20860946e281e7f165384001944f44cdd7146eb6
234687552 <234687552@qq.com>
2017-03-18 18:00:31 +0800

    modify

--- angular-module/app/system/header/HeaderController.js
+++ angular-module/app/system/header/HeaderController.js
@@ -1,38 +1,3 @@
 app.controller('headCtrl', function ($scope, $location) {
-    // Public.
-    // "navItems"存储着导航栏的栏目。
-
-    console.log("headCtrl instance");
-    $scope.navItems = [
-        {
-            id: 1,
-            title: 'Home',
-            locationUrl: '/',
-            href: '#'
-        },
-        {
-            id: 2,
-            title: 'About Me',
-            locationUrl: '/aboutMe',
-            href: '#'
-        },
-        {
-            id: 3,
-            title: 'Management',
-            locationUrl: '/manageHome',
-            href: '#'
-        },
-        {
-            id: 4,
-            title: 'Other',
-            locationUrl: '/manageHome',
-            href: '#'
-        }
-    ];
-
-    // 栏目click时触发的方法。
-    $scope.itemClick = function (id) {
-        $scope.selectedId = id;
-    };
 
 });
